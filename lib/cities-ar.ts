b5f4ddcc947e7c10b941ef447ce106b1a6cd190c
yasserhegazy <yasseranahegazy@gmail.com>
2026-04-09 11:41:56 +0300

    Add additional Arabic city names and implement function to retrieve cities by country ISO2

--- lib/cities-ar.ts
+++ lib/cities-ar.ts
@@ -80,6 +80,8 @@ const CITIES_AR: Record<string, Record<string, string>> = {
     Fujairah: 'الفجيرة', 'Umm Al Quwain': 'أم القيوين',
     'Al Ain': 'العين', 'Umm al-Qaiwain': 'أم القيوين',
     'Ras al-Khaimah': 'رأس الخيمة',
+    'Khor Fakkan': 'خورفكان', Kalba: 'كلباء', Dibba: 'دبا',
+    'Dibba Al-Fujairah': 'دبا الفجيرة',
   },
   EG: {
     Cairo: 'القاهرة', Alexandria: 'الإسكندرية', Giza: 'الجيزة',
@@ -90,6 +92,9 @@ const CITIES_AR: Record<string, Record<string, string>> = {
     Minya: 'المنيا', Hurghada: 'الغردقة', 'Sharm El Sheikh': 'شرم الشيخ',
     Sohag: 'سوهاج', Qena: 'قنا', Damanhur: 'دمنهور',
     'Beni Suef': 'بني سويف', Shibin: 'شبين الكوم',
+    'Kafr El Sheikh': 'كفر الشيخ', Banha: 'بنها', Damietta: 'دمياط',
+    'El Mahalla El Kubra': 'المحلة الكبرى', '6th of October': 'السادس من أكتوبر',
+    Arish: 'العريش', 'Al Arish': 'العريش',
   },
   JO: {
     Amman: 'عمّان', Zarqa: 'الزرقاء', Irbid: 'إربد',
@@ -107,18 +112,37 @@ const CITIES_AR: Record<string, Record<string, string>> = {
     Doha: 'الدوحة', 'Al Wakrah': 'الوكرة', 'Al Khor': 'الخور',
     'Al Rayyan': 'الريان', 'Umm Salal': 'أم صلال',
     'Al Wakra': 'الوكرة', 'Dukhan': 'دخان', 'Messaied': 'مسيعيد',
-    'Ash Shahaniyah': 'الشحانية',
+    'Ash Shahaniyah': 'الشحانية', Lusail: 'لوسيل',
+    'Al Daayen': 'الظعاين',
   },
   BH: {
     Manama: 'المنامة', Riffa: 'الرفاع', Muharraq: 'المحرق',
     'Hamad Town': 'مدينة حمد', 'Isa Town': 'مدينة عيسى',
     Sitra: 'سترة', Zallaq: 'الزلاق', 'Al Hidd': 'الحد',
+    Budaiya: 'البديع', Aali: 'عالي',
+  },
+  DJ: {
+    Djibouti: 'جيبوتي', 'Ali Sabieh': 'علي صبيح', Tadjourah: 'تاجورة',
+    Obock: 'أوبوك', Dikhil: 'دخيل', Arta: 'أرتا',
+  },
+  KM: {
+    Moroni: 'موروني', Mutsamudu: 'موتسامودو', Fomboni: 'فومبوني',
+    Domoni: 'دوموني', 'Mbeni': 'مبيني',
+  },
+  MR: {
+    Nouakchott: 'نواكشوط', Nouadhibou: 'نواذيبو', Rosso: 'روصو',
+    Kaedi: 'كيهيدي', Atar: 'أطار', Kiffa: 'كيفه', Zouerat: 'ازويرات',
+  },
+  SO: {
+    Mogadishu: 'مقديشو', Hargeisa: 'هرجيسا', Bosaso: 'بوصاصو',
+    Kismayo: 'كيسمايو', Baidoa: 'بيدوا', Beledweyne: 'بلدوين',
+    Garowe: 'جروي',
   },
   OM: {
     Muscat: 'مسقط', Salalah: 'صلالة', Sohar: 'صحار',
     Nizwa: 'نزوى', Sur: 'صور', Buraimi: 'البريمي',
     Ibri: 'عبري', Rustaq: 'الرستاق', Khasab: 'خصب',
-    Bahla: 'بهلاء',
+    Bahla: 'بهلاء', Seeb: 'السيب', Barka: 'بركاء',
   },
   LB: {
     Beirut: 'بيروت', Tripoli: 'طرابلس', Sidon: 'صيدا',
@@ -132,52 +156,81 @@ const CITIES_AR: Record<string, Record<string, string>> = {
     Amarah: 'العمارة', Ramadi: 'الرمادي', Baquba: 'بعقوبة',
     Samarra: 'سامراء', Nasiriyah: 'الناصرية', Tikrit: 'تكريت',
     Kut: 'الكوت', Hillah: 'الحلة', Fallujah: 'الفلوجة',
-    Diwaniyah: 'الديوانية',
+    Diwaniyah: 'الديوانية', Kufa: 'الكوفة', Zakho: 'زاخو',
+    Sinjar: 'سنجار',
   },
   SY: {
     Damascus: 'دمشق', Aleppo: 'حلب', Homs: 'حمص',
     Hama: 'حماة', Latakia: 'اللاذقية', Deir: 'دير الزور',
     Daraa: 'درعا', 'Deir ez-Zor': 'دير الزور', Tartus: 'طرطوس',
-    Raqqa: 'الرقة', Qamishli: 'القامشلي',
+    Raqqa: 'الرقة', Qamishli: 'القامشلي', Idlib: 'إدلب',
+    Hasakah: 'الحسكة', Palmyra: 'تدمر',
   },
   LY: {
     Tripoli: 'طرابلس', Benghazi: 'بنغازي', Misrata: 'مصراتة',
     Tarhuna: 'ترهونة', 'Al Bayda': 'البيضاء', Ajdabiya: 'أجدابيا',
     Zawiya: 'الزاوية', Tobruk: 'طبرق', Sabha: 'سبها',
+    Sirte: 'سرت', Derna: 'درنة', Zliten: 'زليتن',
   },
   TN: {
     Tunis: 'تونس', Sfax: 'صفاقس', Sousse: 'سوسة',
     Kairouan: 'القيروان', Bizerte: 'بنزرت', Gabes: 'قابس',
     Ariana: 'أريانة', Gafsa: 'قفصة', Monastir: 'المنستير',
+    Kasserine: 'القصرين', Nabeul: 'نابل', 'Sidi Bouzid': 'سيدي بوزيد',
   },
   MA: {
     Casablanca: 'الدار البيضاء', Rabat: 'الرباط', Fes: 'فاس',
     Marrakesh: 'مراكش', Agadir: 'أكادير', Meknes: 'مكناس',
     Oujda: 'وجدة', Kenitra: 'القنيطرة', Tangier: 'طنجة',
-    Tetouan: 'تطوان',
+    Tetouan: 'تطوان', Safi: 'آسفي', Nador: 'الناظور',
+    'El Jadida': 'الجديدة', Laayoune: 'العيون',
   },
   DZ: {
     Algiers: 'الجزائر', Oran: 'وهران', Constantine: 'قسنطينة',
     Annaba: 'عنابة', Blida: 'البليدة', Batna: 'باتنة',
     Setif: 'سطيف', Tlemcen: 'تلمسان', Biskra: 'بسكرة',
+    Bejaia: 'بجاية', Skikda: 'سكيكدة', Djelfa: 'الجلفة',
+    Mostaganem: 'مستغانم',
   },
   SD: {
     Khartoum: 'الخرطوم', Omdurman: 'أم درمان', Kassala: 'كسلا',
     'Port Sudan': 'بورسودان', Wad: 'ود مدني', Gedaref: 'القضارف',
-    Atbara: 'عطبرة',
+    Atbara: 'عطبرة', Nyala: 'نيالا', 'El Obeid': 'الأبيض',
+    Dongola: 'دنقلا',
   },
   YE: {
     Sanaa: 'صنعاء', Aden: 'عدن', Taiz: 'تعز',
     Hodeidah: 'الحديدة', Mukalla: 'المكلا', Ibb: 'إب',
     Dhamar: 'ذمار', Amran: 'عمران', Hajjah: 'حجة',
+    Marib: 'مأرب', Seiyun: 'سيئون',
   },
   PS: {
     Gaza: 'غزة', Ramallah: 'رام الله', Nablus: 'نابلس',
     Hebron: 'الخليل', Jenin: 'جنين', Tulkarm: 'طولكرم',
     Qalqilya: 'قلقيلية', Bethlehem: 'بيت لحم', Jericho: 'أريحا',
+    Jerusalem: 'القدس', Rafah: 'رفح', 'Khan Yunis': 'خان يونس',
   },
 };
 
+export function getCitiesArByCountry(
+  iso2: string
+): Array<{ nameEn: string; nameAr: string }> {
+  const map = CITIES_AR[iso2.toUpperCase()];
+  if (!map) return [];
+
+  const seen = new Set<string>();
+  const cities = Object.entries(map)
+    .map(([nameEn, nameAr]) => ({ nameEn, nameAr }))
+    .filter((city) => {
+      const key = city.nameAr.toLocaleLowerCase('ar');
+      if (seen.has(key)) return false;
+      seen.add(key);
+      return true;
+    });
+
+  cities.sort((a, b) => a.nameAr.localeCompare(b.nameAr, 'ar'));
+  return cities;
+}
 
 export function getCityAr(iso2: string, cityEn: string): string {
   const map = CITIES_AR[iso2.toUpperCase()];
